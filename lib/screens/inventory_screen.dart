8a68f9c5d54ff25e29556cd81197d6d6ead2469f
Basma <kassimibasma17@gmail.com>
2026-05-18 16:45:31 +0200

    update profile et update images

--- lib/screens/inventory_screen.dart
+++ lib/screens/inventory_screen.dart
@@ -385,34 +385,6 @@ Widget _buildCard(Ingredient ingredient) {
 }
 
 
-
-
-void _showIngredientDetails(Ingredient ingredient) {
-  showDialog(
-    context: context,
-    builder: (_) => AlertDialog(
-      title: Text(ingredient.nom),
-   content: Text(
-  "Quantity: ${ingredient.quantite} ${ingredient.unite}\n"
-  "Category: ${ingredient.type}\n"
-  "Expiration: ${ingredient.dateExpiration.day}/${ingredient.dateExpiration.month}/${ingredient.dateExpiration.year}\n"
-  "Calories: ${ingredient.calories ?? '-'}\n"
-  "Proteins: ${ingredient.proteines ?? '-'}\n"
-  "Carbs: ${ingredient.glucides ?? '-'}\n"
-  "Fats: ${ingredient.lipides ?? '-'}",
-),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text("Close"),
-        ),
-      ],
-    ),
-  );
-}
-
-
-
 void _confirmDelete(Ingredient ingredient) {
   showDialog(
     context: context,
